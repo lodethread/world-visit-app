3e11a402c4f18f40883d3aa705896c1537a53b6e
Magnet Need <shn154fowest@gmail.com>
2026-01-03 01:56:40 +0700

    deleted unnecessary import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite/sqflite.dart';
+// import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'package:world_visit_app/app/app.dart';
