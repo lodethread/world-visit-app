844a1594ee2fae86dda82d1dced03968e76ec1d9
Magnet Need <shn154fowest@gmail.com>
2026-01-03 08:50:24 +0700

    Align formatting and raise iOS deployment target

--- test/features/map/map_page_test.dart
+++ test/features/map/map_page_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 import 'package:world_visit_app/features/map/map_page.dart';
@@ -12,9 +11,6 @@ void main() {
     tester,
   ) async {
     await tester.pumpWidget(const MaterialApp(home: MapPage()));
-    expect(
-      find.text('MAPBOX_ACCESS_TOKEN が設定されていません。'),
-      findsOneWidget,
-    );
+    expect(find.text('MAPBOX_ACCESS_TOKEN が設定されていません。'), findsOneWidget);
   });
 }
